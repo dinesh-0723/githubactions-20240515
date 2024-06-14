8e789d9a9493f2ce3d858c5ae0f1c60b5149067f
Dinesh <57338282+dinesh-0723@users.noreply.github.com>
2024-06-14 23:30:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Welcome To Github Actions Page This Is New"
+    return "Welcome To Github Actions Page This Is New JIRA"
 if __name__ == "__main__":
         app.config['TEMPLATES_AUTO_RELOAD'] = True
         app.run(host='0.0.0.0', debug=True, port=8080)
